7aeaa874c9cac4c769c2cbc8cd908c78eec48b26
Teklu moges <teklumoges482@gmail.com>
2024-09-22 23:56:18 +0300

    login done

--- Frontend/src/app/authentication/login/page.tsx
+++ Frontend/src/app/authentication/login/page.tsx
@@ -6,32 +6,95 @@ import PageContainer from "@/app/(DashboardLayout)/components/container/PageCont
 import Logo from "@/app/(DashboardLayout)/layout/shared/logo/Logo";
 import AuthLogin from "../auth/AuthLogin";
 import Image from "next/image";
+import { FaGoogle } from "react-icons/fa";
+import { RiFacebookCircleFill } from "react-icons/ri";
+import { TbBrandApple } from "react-icons/tb";
 
 const Login2 = () => {
   return (
     <PageContainer title="Login" description="this is Login page">
-      <div>
-        <form action="" className="">
-          <input type="text" className="" />
-          <input type="text" />
-          <select name="" id="">
-            <option value="Farmer">Farmer</option>
-            <option value="Buyer">Buyer</option>
-            <option value="Transporter">Transporter</option>
-          </select>
+      <div className="flex items-center justify-center  min-h-screen  bg-gray-100">
+        <div className="bg-white shadow-lg rounded-lg p-6 md:flex md:space-x-10">
+          <form
+            action=""
+            className="flex flex-col space-y-4 md:space-y-8  md:w-1/2 md:mt-16"
+          >
+            {/* <div className="mb-4 flex justify-center">
+              <Logo />
+            </div> */}
+            <div className="flex justify-center text-bold">
+              <h2 className="text-2xl  text-gray-800 font-bold">Login</h2>
+            </div>
 
-          <div>
-            <input type="checkbox" /> Remember me
+            <input
+              type="text"
+              placeholder="Username"
+              className="bg-gray-200 px-4 py-2 min-w-[330px] max-w-[350px] rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+
+            <input
+              type="password"
+              placeholder="Password"
+              className="bg-gray-200 px-4 py-2 rounded-lg min-w-[330px] max-w-[350px] focus:outline-none focus:ring-2 focus:ring-blue-500"
+            />
+
+            <select className="bg-gray-200 px-4 py-2 min-w-[330px] max-w-[350px] rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500">
+              <option value="Farmer">Farmer</option>
+              <option value="Buyer">Buyer</option>
+              <option value="Transporter">Transporter</option>
+            </select>
+
+            <div className="flex items-center min-w-[330px] max-w-[350px] justify-between">
+              <div>
+                <input type="checkbox" className="mr-2" />
+                <span>Remember me</span>
+              </div>
+              <Link
+                href="/auth/forgot-password"
+                className="text-blue-500 hover:underline"
+              >
+                Forgot Password?
+              </Link>
+            </div>
+
+            <button
+              type="submit"
+              className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition duration-300 max-w-[350px]"
+            >
+              Submit
+            </button>
+            <div>
+              <p className="text-center font-[calibri]">Or login with</p>
+              <div className="flex justify-center space-x-4 my-2">
+                <div className="bg-red-100 w-[76px] h-[30px] rounded-full flex justify-center items-center ">
+                  <FaGoogle className="text-red-500 " />
+                </div>
+                <div
+                  className="bg-blue-100 w-[76px] h-[30px] rounded-full flex justify-center items-center "
+                  style={{ color: "#3b5998" }}
+                >
+                  <RiFacebookCircleFill className="text-blue-500 " />
+                </div>
+                <div
+                  className="bg-[#F3F4F6] w-[76px] h-[30px] rounded-full flex justify-center items-center "
+                  style={{ color: "" }}
+                >
+                  <TbBrandApple />
+                </div>
+              </div>
+            </div>
+          </form>
+
+          <div className="hidden md:block md:w-1/2">
+            <Image
+              src="/images/auth/login.svg"
+              alt="login"
+              width={400}
+              height={400}
+              className="rounded-lg"
+            />
           </div>
-          <Link href="/auth/forgot-password">Forgot Password?</Link>
-          <button type="submit">Submit</button>
-        </form>
-        <Image
-          src="/images/undraw_Login_re_4vu2.svg"
-          alt="login"
-          width={500}
-          height={500}
-        />
+        </div>
       </div>
     </PageContainer>
   );
